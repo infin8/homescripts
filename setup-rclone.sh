4ac7317915fc3b075001bb23c202efd9d520c11b
Animosity022 <earl.texter@gmail.com>
2018-08-16 18:06:36 -0400

    More files

---
+++ setup-rclone.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# All these items are executed as root
+
+# Set the user and groups
+USER=felix
+GROUP=felix
+
+apt -y install unzip
+
+# Standard rclone install
+curl https://rclone.org/install.sh | sudo bash
+
+# Update fuse.conf with the allow other
+
+echo "user_allow_other" >> /etc/fuse.conf
+
+# Create directories for my mounts
+
+mkdir /GD
+mkdir /gmedia
+
+# Fix user/group and chmod
+chown $USER:$GROUP /GD /gmedia
+chmod 775 /GD /gmedia
